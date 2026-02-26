f4a569dea23eba1aa11c32f13c9c0b1057daad8e
austinjclifton <austinjclifton@proton.me>
2026-02-26 17:41:00 -0500

    hardered server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const app = express();
+"use strict";
 
-app.use(express.json());
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = Number(process.env.PORT || 4000);
+const HOST = "0.0.0.0";
 
 // Log all requests
 app.use((req, res, next) => {
@@ -10,19 +14,19 @@ app.use((req, res, next) => {
 });
 
 // Simple GET endpoint
-app.get('/', (req, res) => {
-  console.log('GET request received!');
-  res.json({ message: 'Hello from server!', timestamp: Date.now() });
+app.get("/", (req, res) => {
+  console.log("GET request received!");
+  res.json({ message: "Hello from server!", timestamp: Date.now() });
 });
 
 // Simple POST endpoint
-app.post('/', (req, res) => {
-  console.log('POST request received!');
-  console.log('Data:', req.body);
+app.post("/", (req, res) => {
+  console.log("POST request received!");
+  console.log("Data:", req.body);
   res.json({ success: true, received: req.body });
 });
 
 // Start server
-app.listen(4000, '0.0.0.0', () => {
-  console.log('Server running on port 4000');
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
